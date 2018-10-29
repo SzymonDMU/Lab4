18bbe2325d2f8eaad2cd92d6a3f4ad9bb0046559
Szymon Suchy <P15229207@LEC-ADMIN.dmu.ac.uk>
2018-10-29 10:53:00 +0000

    Lab Has Been completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const menuToggle = document.getElementById('menu-toggle');
+const menuNav = document.getElementById('menu-nav');
+
+function toggleMenu(){
+  console.log("called toggleMenu");
+  menuNav.classList.toggle("menu-toggle")
+  menuToggle.classList.toggle("test")
+
+}
+
+menuToggle.addEventListener("click" , toggleMenu);
